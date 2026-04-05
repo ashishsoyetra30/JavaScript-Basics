ba66b50e472aaa245fc9bbc42e7d99ea15c7f31c
ASHISH SOYETRA <53328534+ashishsoyetra30@users.noreply.github.com>
2026-04-05 17:03:09 +0000

    changed variable name

--- 01-basics/strings.js
+++ 01-basics/strings.js
@@ -1,7 +1,7 @@
-const name = "Ashish"
+const myName = "Ashish"
 const age = 25
 
 // console.log(name + " is " + age + " years old") bad/old way to write not good
 // string interpolation
 
-console.log(`Hello my name is ${name} and I am ${age} years old.`)
+console.log(`Hello my name is ${myName} and I am ${age} years old.`)
