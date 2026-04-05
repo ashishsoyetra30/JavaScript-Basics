4ad2091c79cf5ee51460ae96765c39779c16785c
ASHISH SOYETRA <53328534+ashishsoyetra30@users.noreply.github.com>
2026-04-05 17:05:45 +0000

    added definitions of string methods

--- 01-basics/strings.js
+++ 01-basics/strings.js
@@ -5,3 +5,35 @@ const age = 25
 // string interpolation
 
 console.log(`Hello my name is ${myName} and I am ${age} years old.`)
+
+/*
+1. Extract
+
+slice(start, end) / substring(start, end): Get a portion.
+
+split(separator): Cut into an Array.
+
+2. Search
+
+includes(val): True if found.
+
+indexOf(val) / lastIndexOf(val): Get index position.
+
+startsWith(val) / endsWith(val): Check edges.
+
+3. Modify (Returns New String)
+
+replace(x, y) / replaceAll(x, y): Swap values.
+
+toLowerCase() / toUpperCase(): Change case.
+
+trim(): Remove start/end spaces.
+
+padStart(len, str) / padEnd(len, str): Add filler to reach a length.
+
+4. Access
+
+charAt(index): Get character.
+
+charCodeAt(index): Get UTF-16 code.
+*/
