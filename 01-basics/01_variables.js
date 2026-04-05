58b1d9074b2830b40ee86e2a3d3ea777051ff3a3
ASHISH SOYETRA <53328534+ashishsoyetra30@users.noreply.github.com>
2026-04-05 15:30:18 +0000

    added data ypes

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,8 @@
+const mobile = 91311664059 // const can't be change 
+let name = "ashu" 
+var city = "champa" // don't use as scope problem is there agar loop me koi aur variable banaye same name ke toh wo har jagah change ho jayega
+
+// console.table([to print as table])
+
+//null is a standalone value of type object
+//undefined ka type hi undefined hota hai
