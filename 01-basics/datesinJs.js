97379935fe4ed11d7680b72bf2f2428d7618fa4f
ASHISH SOYETRA <53328534+ashishsoyetra30@users.noreply.github.com>
2026-04-12 18:02:42 +0000

    added a new file named Date

---
+++ 01-basics/datesinJs.js
@@ -0,0 +1,26 @@
+let myDate = new Date()
+
+console.log(myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+
+let myCreatedDate = new Date("04-13-2026")
+console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default', {
+    weekday: "long"
+})
